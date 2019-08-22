06fd156503456ad59dce02df4073f5380c49ac01
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-08-22 06:33:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
   
-
+let interval = null;
+let status = stopped;
 // let a = document.querySelector("#hours").innerHTML;
 // let b = document.querySelector("#minutes").innerHTML;
 // let c = document.querySelector("#seconds").innerHTML;
@@ -51,34 +52,10 @@ function start() {
 
 
 function stop() {
-    // document.querySelector("#display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
-    // if(seconds >= 1){
-    //   document.querySelector("#display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
-    // }
-    if(seconds < 10){
-        displaySeconds = "0" + seconds.toLocaleString();
-    }else{
-        displaySeconds = seconds;
-    }
-
-    if(minutes < 10){
-        displayMinutes = "0" + minutes.toLocaleString();
-    }else{
-        displayMinutes = minutes;
-    }
-
-    if(hours < 10){
-        displayHours = "0" + hours.toLocaleString();
-    }else{
-        displayHours = hours;
-    }
-
-    window.s(stop, 1000);
+    
 }
 
-// function reset() {
-//     document.querySelector("#hours").innerHTML = hours;
-//     document.querySelector("#minutes").innerHTML = minutes;
-//     document.querySelector("#seconds").innerHTML = seconds;
-//     clearInterval(reset);
-// }
+function reset() {
+    document.querySelector("#display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+    window.clearInterval(reset, );
+}
